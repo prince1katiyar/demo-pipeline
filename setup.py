af6deb60ea3aae122719f7432853855f86d4638e
prince katiyar <princekatiyar986@gmail.com>
2023-03-26 19:10:15 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+setup(name="census-income",
+      version="0.0.1",
+      author="prince",
+      author_email="princekatiayr986@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+
+
+      )
